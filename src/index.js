454d42897f0508b7fe230c064bf3b8b39adf06ee
VijayKumar311 <138315705+VijayKumar311@users.noreply.github.com>
2025-10-27 14:43:49 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// File path: src/index.js
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the CI/CD Pipeline! The app is successfully deployed.');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
